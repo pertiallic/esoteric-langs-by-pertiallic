d99d00d067763b4be03d0d0a2099defb8912fd83
pertiallic <pertiallic@gmail.com>
2026-04-10 08:16:15 +0900

    init

---
+++ whitespace/whitespace.py
@@ -0,0 +1,2 @@
+with open("test_formatted.ws","r") as f:
+    print([c for c in f.read()])
---
+++ whitespace/whitespace_formatter.py
@@ -0,0 +1,20 @@
+import os,sys
+import os.path
+def formatter(raw: str) -> str:
+    return raw.replace(" ","").replace("s", " ").replace("t", "\t").replace("n\n", "\n")
+def readfile(path: str | os.PathLike) -> str:
+    with open(path, "r") as f:
+        return f.read()
+def writefile(path: str | os.PathLike, content: str) -> int:
+    with open(path, "w") as f:
+        return f.write(content)
+def fileinsert(path: str, text: str) -> str:
+    name: tuple = os.path.splitext(path)
+    return name[0] + text + name[1]
+def main() -> None:
+    args:list = sys.argv
+    args.append(0)
+    FILEPATH = args[1] or input()
+    writefile(fileinsert(FILEPATH, "_formatted"),formatter(readfile(FILEPATH)))
+if __name__ == "__main__":
+    main()
